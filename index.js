618c4e15deb0d14b828570e1307f5614190cc4e0
Adam Hinton <adam.d.hinton@gmail.com>
2021-10-15 12:22:43 -0400

    5/24 so far

--- index.js
+++ index.js
@@ -28,10 +28,15 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(number) {
+let total = 0;
+  for (let i = 0; i<number+1; i++){
+   total = total +i;
+ }
+return total;
   }
+
+  // console.log(summation(4));
  
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
@@ -56,9 +61,14 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function animalNames(animalArray){
+    const displayNames = [];
+    animalArray.forEach(function(item){
+    return displayNames.push(`name: ${item.animal_name}, scientific: ${item.scientific_name}`)
+    });
+    return displayNames;
   }
+
   
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
@@ -67,9 +77,14 @@ const zooAnimals = [
   For example: ['jackal, asiatic', .....]
   */
 
-  function lowerCaseNames(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+  function lowerCaseNames(animalArray2){
+    const newArray = animalArray2.map(function(item){
+return item.animal_name.toLowerCase();
+    });
+    return newArray;
+  };
+
+  // console.log(lowerCaseNames(zooAnimals));
   
   
   /* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
